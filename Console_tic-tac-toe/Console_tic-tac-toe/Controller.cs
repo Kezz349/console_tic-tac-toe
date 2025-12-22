9bace0fd80b0d6b59bc273dedf07292d4fe5e528
Kezz349 <kevinpariseau@hotmail.com>
2025-12-22 12:43:31 -0500

    .

--- Console_tic-tac-toe/Console_tic-tac-toe/Controller.cs
+++ Console_tic-tac-toe/Console_tic-tac-toe/Controller.cs
@@ -43,45 +43,46 @@ namespace Console_tic_tac_toe
                 View.ShowInfo();
                 View.ShowGrid();
 
+                int playeyTurn = Game.DecidePlayerTurn(Game.turnNumber);
+                
                 ConsoleKeyInfo input = Console.ReadKey();
-
                 switch (input.Key)
                 {
                     case ConsoleKey.D1:
                     case ConsoleKey.NumPad1:
-
+                        Game.PlacePlayer(1, playeyTurn);
                         break;
                     case ConsoleKey.D2:
                     case ConsoleKey.NumPad2:
-
+                        Game.PlacePlayer(2, playeyTurn);
                         break;
                     case ConsoleKey.D3:
                     case ConsoleKey.NumPad3:
-
+                        Game.PlacePlayer(3, playeyTurn);
                         break;
                     case ConsoleKey.D4:
                     case ConsoleKey.NumPad4:
-
+                        Game.PlacePlayer(4, playeyTurn);
                         break;
                     case ConsoleKey.D5:
                     case ConsoleKey.NumPad5:
-
+                        Game.PlacePlayer(5, playeyTurn);
                         break;
                     case ConsoleKey.D6:
                     case ConsoleKey.NumPad6:
-
+                        Game.PlacePlayer(6, playeyTurn);
                         break;
                     case ConsoleKey.D7:
                     case ConsoleKey.NumPad7:
-
+                        Game.PlacePlayer(7, playeyTurn);
                         break;
                     case ConsoleKey.D8:
                     case ConsoleKey.NumPad8:
-
+                        Game.PlacePlayer(8, playeyTurn);
                         break;
                     case ConsoleKey.D9:
                     case ConsoleKey.NumPad9:
-
+                        Game.PlacePlayer(9, playeyTurn);
                         break;
                     case ConsoleKey.Escape:
                         exit = true;
--- Console_tic-tac-toe/Console_tic-tac-toe/Game.cs
+++ Console_tic-tac-toe/Console_tic-tac-toe/Game.cs
@@ -12,16 +12,68 @@ namespace Console_tic_tac_toe
         public static int player1Score = 0;
         public static int player2Score = 0;
 
+        public static int turnNumber = 1;
+
         public static int[,] gameGrid =
         {
             {0, 0, 0},
             {0, 0, 0},
             {0, 0, 0}
         };
-        public static void PlacePlayer()
+        public static void PlacePlayer(int input, int playerTurn)
         {
-
+            switch (input)
+            {
+                case 1:
+                    gameGrid[0, 0] = playerTurn;
+                    turnNumber++;
+                    break;
+                case 2:
+                    gameGrid[0, 1] = playerTurn;
+                    turnNumber++;
+                    break;
+                case 3:
+                    gameGrid[0, 2] = playerTurn;
+                    turnNumber++;
+                    break;
+                case 4:
+                    gameGrid[1, 0] = playerTurn;
+                    turnNumber++;
+                    break;
+                case 5:
+                    gameGrid[1, 1] = playerTurn;
+                    turnNumber++;
+                    break;
+                case 6:
+                    gameGrid[1, 2] = playerTurn;
+                    turnNumber++;
+                    break;
+                case 7:
+                    gameGrid[2, 0] = playerTurn;
+                    turnNumber++;
+                    break;
+                case 8:
+                    gameGrid[2, 1] = playerTurn;
+                    turnNumber++;
+                    break;
+                case 9:
+                    gameGrid[2, 2] = playerTurn;
+                    turnNumber++;
+                    break;
+                default:
+                    break;
+            }
         }
-
+        public static int DecidePlayerTurn(int turnNumber)
+        {
+            if (turnNumber % 2 == 0)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
     }
 }
